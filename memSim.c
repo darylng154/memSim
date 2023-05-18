d01ac53d6c90eb0d3e78181ec5885914ea9ef6ae
Daryl Meng-Lok Ng <dng15@unix3.csc.calpoly.edu>
2023-05-18 14:30:58 -0700

    masking logical address

--- memSim.c
+++ memSim.c
@@ -3,6 +3,17 @@
 #include "tlb.h"
 #include "page_table.h"
 
+// masks the 32-bit logical address to get the 16 rightmost bits (= 8-bit page #, 2 8-bit page offset)
+uint16_t maskFileInts(uint32_t logical_address)
+{
+    uint16_t result = 0;
+    uint32_t mask = 0x0000FFFF;
+
+    result = logical_address & mask;
+
+    return result;
+}
+
 int main(int argc, char *argv[]){
     char *filename;                       /* Holds the file name*/
     uint8_t num_frames = MAX_FRAMES_;     /* Holds the number of frames*/
@@ -23,6 +34,9 @@ int main(int argc, char *argv[]){
 
     // tlb_table->num_entries = MAX_TLB_SIZE_;
     // page_table->num_entries = MAX_FRAME_;
+
+    uint32_t test = 0x12345678;
+    printf("mask test: %0x", maskFileInts(test));
 
     // 0 is printDetails: for more details in later implementation
     if(verbosity){
