4ae6380bc34218c88ca5615baae453d9e9604fd9
Daryl Ng <daryl.ng154@gmail.com>
2023-05-18 17:55:07 -0700

    moved main mask functions into address.h

--- memSim.c
+++ memSim.c
@@ -53,65 +53,6 @@ void readFile(FILE *file_ptr, unsigned int **address_list, unsigned int *address
     return;
 }
 
-// masks the 32-bit logical address to get the 16 rightmost bits (= 8-bit page #, 2 8-bit page offset)
-uint16_t maskFileInts(uint32_t logical_address)
-{
-    uint16_t result = 0;
-    uint32_t mask = 0x0000FFFF;
-
-    result = logical_address & mask;
-
-    return result;
-}
-
-// masks the 16 rightmost bits to get the left 8-bits = page #
-uint8_t maskPageNum(uint16_t right_most_bits)
-{
-    uint8_t result = 0;
-    uint16_t mask = 0x0000FF00;
-
-    result = (right_most_bits & mask) >> 8;
-
-    return result;
-}
-
-// masks the 16 rightmost bits to get the left 8-bits = page #
-uint8_t maskOffset(uint16_t right_most_bits)
-{
-    uint8_t result = 0;
-    uint16_t mask = 0x000000FF;
-
-    result = right_most_bits & mask;
-
-    return result;
-}
-
-void runAlgorithm(uint16_t* right_most_bits, uint8_t algorithm)
-{
-    switch(algorithm)
-    {
-        case 0:
-            if(verbosity)
-                printf("Running FIFO\n");
-            break;
-
-        case 1:
-            if(verbosity)
-                printf("Running LRU\n");
-            break;
-
-        case 2:
-            if(verbosity)
-                printf("Running OPT\n");
-            break;
-
-        default:
-            perror("#ERROR: runAlgorithm defaulted, %u does not exist\n");
-            exit(EXIT_FAILURE);
-            break;
-    }
-}
-
 int main(int argc, char *argv[]){
     char *filename;                       /* Name of input file*/
     uint8_t num_frames = MAX_FRAMES_;     /* User specified number of frames*/
